8968c0b4f3b4d1b74cd164d82c98383ab6acf286
Gus Smith <guscomps@gmail.com>
2025-05-01 12:43:23 -0700

    update synth script

--- vivado/synth_script.tcl
+++ vivado/synth_script.tcl
@@ -1,7 +1,8 @@
-read_verilog -sv example.sv
+set script_path [ file dirname [ file normalize [ info script ] ] ]
+read_verilog -sv "$script_path/../sub_mul.sv"
 set_part xcu200-fsgd2104-2-e
-read_xdc example.xdc
-synth_design -top presubmul_3_stage_unsigned_16_bit -mode out_of_context -directive AreaMultThresholdDSP
+read_xdc "$script_path/constraints.xdc"
+synth_design -top sub_mul -mode out_of_context -directive AreaMultThresholdDSP
 opt_design
-write_verilog -force example_vivado_synth.sv
+write_verilog -force "$script_path/vivado_out.sv"
 report_utilization

a574994368fd4eef5d97aa39eb010b3ee458ad9f
Gus Smith <guscomps@gmail.com>
2025-05-01 12:46:27 -0700

    Vivado out

---
+++ vivado/vivado_out.sv
@@ -0,0 +1,189 @@
+// Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+// --------------------------------------------------------------------------------
+// Tool Version: Vivado v.2022.1 (lin64) Build 3526262 Mon Apr 18 15:47:01 MDT 2022
+// Date        : Thu May  1 12:43:31 2025
+// Host        : jura running 64-bit Ubuntu 20.04.6 LTS
+// Command     : write_verilog -force /home/gussmith/2025-latchup-demo/vivado/vivado_out.sv
+// Design      : sub_mul
+// Purpose     : This is a Verilog netlist of the current design or from a specific cell of the design. The output is an
+//               IEEE 1364-2001 compliant Verilog HDL file that contains netlist information obtained from the input
+//               design files.
+// Device      : xcu200-fsgd2104-2-e
+// --------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+(* NETLIST_CHECKSUM = "cd0a0d52" *) (* use_dsp = "yes" *) 
+(* STRUCTURAL_NETLIST = "yes" *)
+module sub_mul
+   (clk,
+    a,
+    b,
+    d,
+    out);
+  input clk;
+  input [15:0]a;
+  input [15:0]b;
+  input [15:0]d;
+  output [15:0]out;
+
+  wire \<const0> ;
+  wire \<const1> ;
+  wire [15:0]a;
+  wire [15:0]b;
+  wire clk;
+  wire [15:0]d;
+  wire [15:0]out;
+  wire [15:0]stage0_reg__0;
+
+  GND GND
+       (.G(\<const0> ));
+  VCC VCC
+       (.P(\<const1> ));
+  (* KEEP_HIERARCHY = "yes" *) 
+  (* METHODOLOGY_DRC_VIOS = "{SYNTH-12 {cell *THIS*}}" *) 
+  DSP48E2 #(
+    .ACASCREG(0),
+    .ADREG(1),
+    .ALUMODEREG(0),
+    .AMULTSEL("A"),
+    .AREG(0),
+    .AUTORESET_PATDET("NO_RESET"),
+    .AUTORESET_PRIORITY("RESET"),
+    .A_INPUT("DIRECT"),
+    .BCASCREG(0),
+    .BMULTSEL("B"),
+    .BREG(0),
+    .B_INPUT("DIRECT"),
+    .CARRYINREG(0),
+    .CARRYINSELREG(0),
+    .CREG(0),
+    .DREG(1),
+    .INMODEREG(0),
+    .MASK(48'h3FFFFFFFFFFF),
+    .MREG(0),
+    .OPMODEREG(0),
+    .PATTERN(48'h000000000000),
+    .PREADDINSEL("A"),
+    .PREG(1),
+    .RND(48'h000000000000),
+    .SEL_MASK("MASK"),
+    .SEL_PATTERN("PATTERN"),
+    .USE_MULT("NONE"),
+    .USE_PATTERN_DETECT("NO_PATDET"),
+    .USE_SIMD("ONE48"),
+    .USE_WIDEXOR("FALSE"),
+    .XORSIMD("XOR24_48_96")) 
+    stage0_reg
+       (.A({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .ACIN({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .ALUMODE({\<const0> ,\<const0> ,\<const1> ,\<const1> }),
+        .B({\<const0> ,\<const0> ,a}),
+        .BCIN({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .C({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,d}),
+        .CARRYCASCIN(\<const0> ),
+        .CARRYIN(\<const0> ),
+        .CARRYINSEL({\<const0> ,\<const0> ,\<const0> }),
+        .CEA1(\<const0> ),
+        .CEA2(\<const0> ),
+        .CEAD(\<const0> ),
+        .CEALUMODE(\<const0> ),
+        .CEB1(\<const0> ),
+        .CEB2(\<const0> ),
+        .CEC(\<const0> ),
+        .CECARRYIN(\<const0> ),
+        .CECTRL(\<const0> ),
+        .CED(\<const0> ),
+        .CEINMODE(\<const0> ),
+        .CEM(\<const0> ),
+        .CEP(\<const1> ),
+        .CLK(clk),
+        .D({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .INMODE({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .MULTSIGNIN(\<const0> ),
+        .OPMODE({\<const0> ,\<const0> ,\<const0> ,\<const1> ,\<const1> ,\<const0> ,\<const0> ,\<const1> ,\<const1> }),
+        .P(stage0_reg__0),
+        .PCIN({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .RSTA(\<const0> ),
+        .RSTALLCARRYIN(\<const0> ),
+        .RSTALUMODE(\<const0> ),
+        .RSTB(\<const0> ),
+        .RSTC(\<const0> ),
+        .RSTCTRL(\<const0> ),
+        .RSTD(\<const0> ),
+        .RSTINMODE(\<const0> ),
+        .RSTM(\<const0> ),
+        .RSTP(\<const0> ));
+  (* KEEP_HIERARCHY = "yes" *) 
+  DSP48E2 #(
+    .ACASCREG(0),
+    .ADREG(1),
+    .ALUMODEREG(0),
+    .AMULTSEL("A"),
+    .AREG(0),
+    .AUTORESET_PATDET("NO_RESET"),
+    .AUTORESET_PRIORITY("RESET"),
+    .A_INPUT("DIRECT"),
+    .BCASCREG(1),
+    .BMULTSEL("B"),
+    .BREG(1),
+    .B_INPUT("DIRECT"),
+    .CARRYINREG(0),
+    .CARRYINSELREG(0),
+    .CREG(1),
+    .DREG(1),
+    .INMODEREG(0),
+    .MASK(48'h3FFFFFFFFFFF),
+    .MREG(1),
+    .OPMODEREG(0),
+    .PATTERN(48'h000000000000),
+    .PREADDINSEL("A"),
+    .PREG(1),
+    .RND(48'h000000000000),
+    .SEL_MASK("MASK"),
+    .SEL_PATTERN("PATTERN"),
+    .USE_MULT("MULTIPLY"),
+    .USE_PATTERN_DETECT("NO_PATDET"),
+    .USE_SIMD("ONE48"),
+    .USE_WIDEXOR("FALSE"),
+    .XORSIMD("XOR24_48_96")) 
+    stage2_reg
+       (.A({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,stage0_reg__0}),
+        .ACIN({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .ALUMODE({\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .B({\<const0> ,\<const0> ,b}),
+        .BCIN({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .C({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .CARRYCASCIN(\<const0> ),
+        .CARRYIN(\<const0> ),
+        .CARRYINSEL({\<const0> ,\<const0> ,\<const0> }),
+        .CEA1(\<const0> ),
+        .CEA2(\<const0> ),
+        .CEAD(\<const0> ),
+        .CEALUMODE(\<const0> ),
+        .CEB1(\<const0> ),
+        .CEB2(\<const1> ),
+        .CEC(\<const0> ),
+        .CECARRYIN(\<const0> ),
+        .CECTRL(\<const0> ),
+        .CED(\<const0> ),
+        .CEINMODE(\<const0> ),
+        .CEM(\<const1> ),
+        .CEP(\<const1> ),
+        .CLK(clk),
+        .D({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .INMODE({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .MULTSIGNIN(\<const0> ),
+        .OPMODE({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const1> ,\<const0> ,\<const1> }),
+        .P(out),
+        .PCIN({\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> ,\<const0> }),
+        .RSTA(\<const0> ),
+        .RSTALLCARRYIN(\<const0> ),
+        .RSTALUMODE(\<const0> ),
+        .RSTB(\<const0> ),
+        .RSTC(\<const0> ),
+        .RSTCTRL(\<const0> ),
+        .RSTD(\<const0> ),
+        .RSTINMODE(\<const0> ),
+        .RSTM(\<const0> ),
+        .RSTP(\<const0> ));
+endmodule
